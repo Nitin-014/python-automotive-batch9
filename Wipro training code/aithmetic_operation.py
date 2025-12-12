c7a2118c7560e8974a9d531da5e4e9acd375e57d
Nitin-014 <nitinkumar.ais@gmail.com>
2025-12-12 22:54:01 +0530

    first commit

---
+++ Wipro training code/aithmetic_operation.py
@@ -0,0 +1,29 @@
+#ADDITION
+
+
+print("Addition of two numbers")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+add =a + b
+print("Addition is:", add)
+
+
+#SUBTRACTION
+
+print("\nSubtraction of two numbers")
+c= int(input("Enter first number: "))
+d= int(input("Enter second number: "))
+sub=c-d
+print("Subtraction is:", sub)
+
+
+
+
+
+#MULTIPLICATION
+
+print("\nMultiplication of two numbers")
+m = int(input("Enter first number: "))
+n = int(input("Enter second number: "))
+mul = m * n
+print("Multiplication is:", mul)
